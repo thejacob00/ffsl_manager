31f0477fe0e4c081cce93e302eb8459fb266dd04
thejacob00 <california_dreamer_329@yahoo.com>
2022-01-18 21:07:19 -0500

    Fixed bug in database pool; got local connection up and running

--- routes/user.js
+++ routes/user.js
@@ -4,14 +4,13 @@ const router = express.Router();
 
 router.get('/:id', async function(req, res){
     try{
-        const sqlQuery = 'SELECT first_name, last_name FROM items WHERE id=?';
+        const sqlQuery = 'SELECT first_name, last_name FROM owners WHERE id=?';
         const rows = await pool.query(sqlQuery, req.params.id);
         res.status(200).json(rows);
     }
     catch (error) {
         res.status(400).send(error.message);
     }
-    res.status(200).json({id:req.params.id});
 })
 
 module.exports = router;
